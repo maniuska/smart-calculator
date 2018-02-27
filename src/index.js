27d35c91159efaa9645ceca91d3885af1882ee4b
marina <marina.shatinskaya@gmail.com>
2018-02-27 18:54:27 +0300

    solution 1

--- src/index.js
+++ src/index.js
@@ -1,27 +1,77 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
-
-  add(number) {
-    // your implementation
-  }
+ 
+    constructor(initialValue) {
+      this.pastOper = [initialValue];
+    }
   
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+      this.pastOper.push("+",number);
+      return this; 
+    }
+    
+    subtract(number) {
+      this.pastOper.push("-",number);
+      return this;
+    }
+  
+    multiply(number) {
+      this.pastOper.push("*",number);
+      return this;
+    }
+  
+    devide(number) {
+      this.pastOper.push("/",number);
+      return this;
+    }
+  
+    pow(number) {
+      this.pastOper.push("^",number);
+      return this;
+    }
+    
+    toString() {
+      
+       for (var i=1; i<this.pastOper.length-1; i+=2){
+        if (this.pastOper[i]=="^"){
+          this.pastOper[i-1]=Math.pow(this.pastOper[i-1],this.pastOper[i+1]);
+          this.pastOper.splice(i,2);
+          i-=2;
+        }
+      }
+      
+       //check * /
+      for (var i=1; i<this.pastOper.length-1; i+=2){
+        if (this.pastOper[i]=="*"){
+          this.pastOper[i-1]=this.pastOper[i-1]*this.pastOper[i+1];
+          this.pastOper.splice(i,2);
+          i-=2;
+        }
+         if (this.pastOper[i]=="/"){
+          this.pastOper[i-1]=this.pastOper[i-1]/this.pastOper[i+1];
+          this.pastOper.splice(i,2);
+          i-=2;
+        }
+      }
+      //check + - 
+      for (var i=1; i<this.pastOper.length-1; i+=2){
+        if (this.pastOper[i]=="+"){
+          this.pastOper[i-1]=this.pastOper[i-1]+this.pastOper[i+1];
+          this.pastOper.splice(i,2);
+          i-=2;
+        }
+         if (this.pastOper[i]=="-"){
+          this.pastOper[i-1]=this.pastOper[i-1]-this.pastOper[i+1];
+          this.pastOper.splice(i,2);
+          i-=2;
+        }
+      }
+      console.log("value is"+this.pastOper[0]);
+      var value=this.pastOper[0];
+      return value;
+    }
+    
+  
 
-  multiply(number) {
-    // your implementation
-  }
-
-  devide(number) {
-    // your implementation
-  }
-
-  pow(number) {
-    // your implementation
-  }
 }
 
 module.exports = SmartCalculator;
